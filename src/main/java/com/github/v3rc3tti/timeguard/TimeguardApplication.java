4024330803ee04d5ecf8b3a4b0a92fbf3b2ce4e5
Uladzislau <gorbatvladislav@gmail.com>
2022-08-03 15:30:12 +0300

    first commit

---
+++ src/main/java/com/github/v3rc3tti/timeguard/TimeguardApplication.java
@@ -0,0 +1,13 @@
+package com.github.v3rc3tti.timeguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/v3rc3tti/timeguard/TimeguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.v3rc3tti.timeguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
